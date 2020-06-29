3c4177b9d9397372626a7cd3bf9a80ee2251ea1e
Gernot H. Reichel <gernot.reichel@me.com>
2020-06-29 14:02:33 +0200

    added comment

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -11,6 +11,9 @@ export class TabsPage {
     this.startOnlineStatusThread();
   }
 
+  /**
+   * Repetitive task (check server status, ...)
+   */
   private startOnlineStatusThread() {
     this.statusSrv.ping();
     setInterval(() => {
