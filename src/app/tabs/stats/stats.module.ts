162f75ca3e31b9f87cf19bd69075a33584c11d8d
Gernot H. Reichel <gernot.reichel@me.com>
2021-04-19 19:20:54 +0200

    imports rearranged

--- src/app/tabs/stats/stats.module.ts
+++ src/app/tabs/stats/stats.module.ts
@@ -1,12 +1,11 @@
-import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { OnlineStatusComponentModule } from '../../online-status/online-status.module';
+import { StatsPageRoutingModule } from './stats-routing.module';
 import { StatsPage } from './stats.page';
 
-import { StatsPageRoutingModule } from './stats-routing.module';
-import { OnlineStatusComponentModule } from '../../online-status/online-status.module';
 
 @NgModule({
   imports: [
