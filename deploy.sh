a926b207e0e681e35188f7082013595248e36a1b
Gernot H. Reichel <gernot.reichel@me.com>
2021-12-27 19:48:49 +0100

    updated some dependencies

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,15 @@ export HOST=qnap-nas
 export USER=nobio
 export TARGET_DIR=/share/Web/timetracker
 
+echo ========= build project ========
 ionic build --prod                    
 tar cvfz www.tgz www/*
+
+echo ========= deploy project to qnap ========
 ssh $USER@$HOST "rm -rf $TARGET_DIR/*"
 scp www.tgz $USER@$HOST:$TARGET_DIR
 ssh $USER@$HOST "tar xvfz $TARGET_DIR/www.tgz --directory $TARGET_DIR/; mv $TARGET_DIR/www/* $TARGET_DIR/; rm -rf $TARGET_DIR/www*"
 rm www.tgz
+
+echo ========= deploy project to firebase ========
 firebase deploy
