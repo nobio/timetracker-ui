83e9d4937d2933ff58a5b3b40fd9affb537e3735
nobio <gernot.reichel@me.com>
2022-12-13 09:26:19 +0100

    added enabled to model

--- src/app/models/geo-fence.ts
+++ src/app/models/geo-fence.ts
@@ -1,5 +1,6 @@
 export class GeoFence {
-   id: string
+   id: string;
+   enabled: boolean;
    longitude: number;
    latitude: number;
    radius: number;
@@ -7,8 +8,9 @@ export class GeoFence {
    isCheckedIn: boolean = false;
    lastChange: Date;
 
-   constructor(id: string = '', longitude: number = 0, latitude: number = 0, radius: number = 0, description: string = '', isCheckedIn: boolean = false, lastChange: Date = new Date) {
+   constructor(id: string = '', enabled = false, longitude: number = 0, latitude: number = 0, radius: number = 0, description: string = '', isCheckedIn: boolean = false, lastChange: Date = new Date) {
       this.id = id;
+      this.enabled = enabled;
       this.longitude = longitude;
       this.latitude = latitude;
       this.radius = radius;
