d2d815785988e7a8318bb368de25f9d304c2b267
Gernot H. Reichel <gernot.reichel@me.com>
2023-02-09 15:45:16 +0100

    switch to http

--- src/environments/environment.docker.ts
+++ src/environments/environment.docker.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://h3001733.stratoserver.net:30043',
-  websocketUrl: 'https://h3001733.stratoserver.net:30043/ws',
+  baseUrl: 'http://h3001733.stratoserver.net:30030',
+  websocketUrl: 'http://h3001733.stratoserver.net:30030/ws',
 };
