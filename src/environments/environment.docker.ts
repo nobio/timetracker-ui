663fe80d2a3f2c9c66923f40211d590c897d1e09
Gernot H. Reichel <gernot.reichel@me.com>
2023-02-09 15:00:59 +0100

    adapted env for docker

--- src/environments/environment.docker.ts
+++ src/environments/environment.docker.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'timetracker',
-  websocketUrl: 'wss://timetracker/ws',
+  baseUrl: 'https://h3001733.stratoserver.net:30043',
+  websocketUrl: 'https://h3001733.stratoserver.net:30043/ws',
 };
