b5c6871264771e240eede44e9fdde6bfc5491989
Gernot H. Reichel <gernot.reichel@me.com>
2024-04-28 18:58:59 +0200

    added new build target for hetzner

---
+++ src/environments/environment.hetzner.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  baseUrl: 'http://88.198.110.159',
+  websocketUrl: 'wss://88.198.110.159/ws',
+  env: 'hetzner',
+  apiKey: 'Loak38Jue7Gtw5stGabYx6xb4sgXgsFS',
+};
