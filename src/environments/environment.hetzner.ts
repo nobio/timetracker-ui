8300bd62d7d782aef4f6e13c234ebc876f16b8ed
Gernot H. Reichel <gernot.reichel@me.com>
2024-04-28 19:09:15 +0200

    changed port for timetracker on hetzner build target

--- src/environments/environment.hetzner.ts
+++ src/environments/environment.hetzner.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://88.198.110.159',
-  websocketUrl: 'wss://88.198.110.159/ws',
+  baseUrl: 'http://88.198.110.159:30030',
+  websocketUrl: 'wss://88.198.110.159:30043/ws',
   env: 'hetzner',
   apiKey: 'Loak38Jue7Gtw5stGabYx6xb4sgXgsFS',
 };
